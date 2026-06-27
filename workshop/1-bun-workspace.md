47da30451056935c6d3d9bd31a5f437158d9a00c
pathompon saensurin <byrdcnx@hotmail.com>
2026-06-27 16:18:17 +0700

    create workshop 1

---
+++ workshop/1-bun-workspace.md
@@ -0,0 +1,285 @@
+# Bun Workspace (Monorepo)
+
+คือ ฟีเจอร์ที่ช่วยให้เราสามารถจัดการ หลายๆ โปรเจกต์ (เช่น หน้าบ้าน และ หลังบ้าน) รวมไว้ในโฟลเดอร์ (Repository) เดียวกันได้ โดยใช้ Bun เป็นตัวบริหารจัดการ  
+แทนที่จะแยกโฟลเดอร์ Frontend และ Backend ออกจากกันเป็นคนละโฟลเดอร์หรือคนละที่ เราจับมันมารวมกันอยู่ในที่เดียว
+
+```
+my-project/
+├── package.json        # ตั้งค่า workspace: ["apps/*"]
+├── apps/
+│   ├── frontend/       # โปรเจกต์ Nuxt.js
+│   └── backend/        # โปรเจกต์ Elysia.js
+```
+
+# 🚀 ข้อดีของ bun Workspace
+
+1. Single Install (ลงตัวเดียว จบ): สั่งคำสั่ง bun install แค่ครั้งเดียวที่โฟลเดอร์นอกสุด (Root) ตัว Bun จะวิ่งไปดาวน์โหลดและจัดการ Node Modules ให้ทุกแอปใน Monorepo ทันที แถมแชร์ Library ร่วมกันได้ ทำให้ประหยัดพื้นที่ฮาร์ดดิสก์
+2. Easy Code & Type Sharing: ฝั่ง Frontend สามารถวิ่งไปดึง Type หรือแชร์โค้ดจากฝั่ง Backend มาใช้ได้โดยตรงผ่านคำสั่ง import ธรรมดา ไม่ต้องทำขั้นตอน Compile หรือ Public ขึ้น npm registry ให้วุ่นวาย
+
+# Workshop
+
+#### โครงสร้างโปรเจค
+
+```
+attractions/
+├── package.json        # ตั้งค่า workspace: ["apps/*"]
+├── apps/
+│   ├── app/            # โปรเจกต์ Frontend ด้วย Nuxt.js
+│   └── api/            # โปรเจกต์ Backend ด้วย Elysia.js
+│── packages/           # โฟลเดอร์ packages สำหรับ share resource ในโปรเจค
+│   ├── types/          # โฟลเดอร์ types สำหรับเก็บ share type
+│   ├── i18n/           # โฟลเดอร์ i18n สำหรับเก็บชุดข้อความ
+```
+
+## เริ่มต้นสร้าง Workspace
+
+```
+ิีืbun init
+
+---
+
+? Select a project template - Press return to submit.
+❯   Blank
+    React
+    Library
+```
+
+### ตั้งค่า Monorepo เบื้องต้น
+
+package.json
+
+```
+{
+  //ตั้งชื่อ package หลัก
+  "name": "attractionsmonorepo",
+
+  "module": "index.ts",
+  "type": "module",
+  "private": true,
+
+  //ประกาศพื้นที่ Workspaces
+  "workspaces": [
+    "apps/*",
+    "packages/*"
+  ],
+
+  "devDependencies": {
+    "@types/bun": "latest"
+  },
+  "peerDependencies": {
+    "typescript": "^5"
+  }
+}
+
+```
+
+tsconfig.json
+
+```
+{
+  "compilerOptions": {
+    // Environment setup & latest features
+    "lib": ["ESNext", "dom"],
+    "target": "ESNext",
+    "module": "Preserve",
+    "moduleDetection": "force",
+    "jsx": "preserve",
+    "allowJs": true,
+
+    // Bundler mode
+    "moduleResolution": "bundler",
+    "allowImportingTsExtensions": true,
+    "verbatimModuleSyntax": true,
+    "noEmit": true,
+
+    // Best practices
+    "strict": true,
+    "skipLibCheck": true,
+    "noFallthroughCasesInSwitch": true,
+    "noUncheckedIndexedAccess": true,
+    "noImplicitOverride": true,
+
+    // Some stricter flags (disabled by default)
+    "noUnusedLocals": false,
+    "noUnusedParameters": false,
+    "noPropertyAccessFromIndexSignature": false,
+
+    "isolatedModules": true,
+    "esModuleInterop": true
+  }
+}
+
+```
+
+### สร้างโปรเจค Frontend
+
+เข้าไปที่โฟลเดอร์ apps/ แล้วสร้างโฟลเดอร์ชื่อ app/ ขึ้นมาจากนั้นติดตั้ง Nuxt.js
+
+```
+bun create nuxt@latest .
+
+Which template would you like to use?
+❯ minimal – Minimal setup for Nuxt 4
+
+The directory .../workshop/attractions/apps/app already exists. What would you like to do?
+❯ Override its contents
+
+Which package manager would you like to use?
+❯ bun (current)
+
+Would you like to browse and install modules?
+❯ No
+```
+
+แก้ไขไฟล์ package.json ของ app
+
+```
+{
+  //ตั้งชื่อ package ของ app
+  "name": "@attraction/app",
+
+  //สามารถกำหนด version ให้ package ของเราได้ หากต้องการ
+  "version": "1.0.0",
+  "type": "module",
+  "private": true,
+  "scripts": {
+    "build": "nuxt build",
+    "dev": "nuxt dev",
+    "generate": "nuxt generate",
+    "preview": "nuxt preview",
+    "postinstall": "nuxt prepare"
+  },
+  "dependencies": {
+    "nuxt": "^4.4.8",
+    "vue": "^3.5.38",
+    "vue-router": "^5.1.0"
+  }
+}
+
+```
+
+### สร้างโปรเจค Backend
+
+เข้าไปที่โฟลเดอร์ apps/ แล้วสร้างโฟลเดอร์ชื่อ api/ จากนั้นติดตั้ง elysia.js
+
+```
+bun create elysia .
+```
+
+แก้ไขไฟล์ package.json ของ api
+
+```
+{
+  //ตั้งชื่อ package ของ api
+  "name": "@attraction/api",
+  "version": "1.0.50",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "dev": "bun run --watch src/index.ts"
+  },
+  "dependencies": {
+    "elysia": "latest"
+  },
+  "devDependencies": {
+    "bun-types": "latest"
+  },
+  "module": "src/index.js"
+}
+
+```
+
+### เตรียม packages สำหรับ share resource ให้กับ Frontend และ Backend
+
+เข้าไปที่โฟลเดอร์ packages/ แล้วทำการสร้างโฟลเดอร์ขึ้นมา 2 โฟลเดอร์ดังนี้
+
+```
+...
+packages/           # โฟลเดอร์ packages สำหรับ share resource ในโปรเจค
+├── types/          # โฟลเดอร์ types สำหรับเก็บ share type
+├── i18n/           # โฟลเดอร์ i18n สำหรับเก็บชุดข้อความ
+```
+
+เข้าไปในโฟลเดอร์ i18n ทำการสร้างโฟเดอร์และไฟล์ดังนี้
+
+```
+packages/
+├── i18n/
+│   ├── locales         # โฟลเดอร์เก็บชุดข้อความแต่ละภาษา
+│   │   ├ th.json       # ชุดข้อความภาษาไทย
+│   ├── index.ts        # สำหรับ export ข้อมูลออกไปใช้งาน
+│   ├── package.json
+```
+
+package.json
+
+```
+{
+  //ชื่อ package
+  "name": "@attraction/i18n",
+  "version": "1.0.0",
+  "private": true,
+  "type": "module",
+
+  //ไฟล์หลักของโมดูลนี้ เมื่อแอปอื่นพยายามเรียกใช้โมดูลนี้ ตัว Runtime (เช่น Bun) จะวิ่งมาอ่านไฟล์ ./index.ts นี้เป็นไฟล์แรกเพื่อดึงฟังก์ชันด้านในไปใช้งาน
+  "main": "./index.ts",
+  "types": "./index.ts",
+
+  //กำหนดไฟล์ที่จะถูกส่งออกของโมดูลนี้ (หากคนนอกจะเรียกใช้แพ็กเกจนี้ (จุด .) ให้เข้าถึงได้เฉพาะไฟล์ ./index.ts เท่านั้น)
+  "exports": {
+    ".": "./index.ts"
+  }
+}
+
+```
+
+index.ts
+
+```
+import * as th from "./locales/th.json";
+
+export { th };
+
+```
+
+เข้าไปในโฟลเดอร์ types ทำการสร้างไฟล์ดังนี้
+
+```
+types/
+├── index.ts
+├── package.json
+```
+
+package.json
+
+```
+{
+  //ชื่อ package
+  "name": "@attraction/types",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "index.ts",
+  "private": true,
+  "exports": {
+    ".": "./index.ts"
+  }
+}
+
+
+```
+
+เปิดไฟล์ attractions/tsconfig.json เพิ่ม path mapping (หรือการทำ alias) หรือก็คือการตั้งชื่อเล่น (Shortcut) ให้กับตำแหน่งของไฟล์ในโปรเจกต์" เพื่อให้เราสามารถเขียนคำสั่ง import โค้ดข้ามโฟลเดอร์ได้สั้น สะอาด และเป็นระบบ
+
+tsconfig.ts
+
+```
+{
+  "compilerOptions": {
+    //ตั้ง path shortcut ให้ packages ของเรา
+    "paths": {
+      "@attraction/types": ["./packages/types/index.ts"],
+      "@attraction/i18n": ["./packages/i18n/index.ts"]
+    },
+    ...
+  }
+}
+```
---
+++ workshop/db/attractions_db.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 27, 2026 at 09:46 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `attractions_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'ศาสนสถาน'),
+(2, 'ประวัติศาสตร์'),
+(3, 'วัฒนธรรม'),
+(4, 'ธรรมชาติ'),
+(5, 'เกาะ/ทะเล'),
+(6, 'ธรรมชาติ/วัฒนธรรม');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `places`
+--
+
+CREATE TABLE `places` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `open_time` varchar(100) DEFAULT NULL,
+  `ticket_price` varchar(100) DEFAULT NULL,
+  `image_url` varchar(500) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `userId` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `places`
+--
+
+INSERT INTO `places` (`id`, `name`, `location`, `description`, `category_id`, `open_time`, `ticket_price`, `image_url`, `created_at`, `updated_at`, `userId`) VALUES
+(1, 'วัดพระแก้ว', 'กรุงเทพมหานคร', 'วัดคู่บ้านคู่เมืองที่ประดิษฐานพระแก้วมรกต', 1, '08:30 - 15:30', '500 บาท (ต่างชาติ)', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(2, 'พระบรมมหาราชวัง', 'กรุงเทพมหานคร', 'พระราชวังหลวงของไทย มีสถาปัตยกรรมงดงาม', 2, '08:30 - 15:30', '500 บาท', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(3, 'วัดอรุณราชวราราม', 'กรุงเทพมหานคร', 'วัดชื่อดังริมแม่น้ำเจ้าพระยา มีพระปรางค์สูงเด่น', 1, '08:00 - 17:30', '100 บาท', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(4, 'ตลาดร่มหุบ', 'สมุทรสงคราม', 'ตลาดที่มีรถไฟวิ่งผ่านกลางตลาด สร้างความตื่นเต้น', 3, '06:00 - 17:00', 'ฟรี', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(5, 'ตลาดน้ำอัมพวา', 'สมุทรสงคราม', 'ตลาดน้ำยามเย็นยอดนิยมริมคลองอัมพวา', 3, '16:00 - 21:00 (ศุกร์ - อาทิตย์)', 'ฟรี', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(6, 'เขาใหญ่', 'นครราชสีมา', 'อุทยานแห่งชาติที่มีธรรมชาติสวยงามและสัตว์ป่า', 4, '06:00 - 18:00', '40 บาท (คนไทย) / 400 บาท (ต่างชาติ)', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(7, 'ดอยอินทนนท์', 'เชียงใหม่', 'ยอดเขาสูงที่สุดในประเทศไทย มีอากาศเย็นตลอดปี', 4, '06:00 - 18:00', '50 บาท / 300 บาท', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(8, 'ภูชี้ฟ้า', 'เชียงราย', 'จุดชมวิวทะเลหมอกยอดนิยม ชมพระอาทิตย์ขึ้น', 4, 'ตลอดวัน', 'ฟรี', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(9, 'เกาะพะงัน', 'สุราษฎร์ธานี', 'เกาะยอดนิยมสำหรับ Full Moon Party และธรรมชาติ', 5, 'ตลอดวัน', 'ฟรี (ค่าข้ามเรือ)', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(10, 'เกาะสมุย', 'สุราษฎร์ธานี', 'เกาะท่องเที่ยวชื่อดังระดับโลก มีชายหาดสวยงาม', 5, 'ตลอดวัน', 'ฟรี (ค่าข้ามเรือ)', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(11, 'เกาะพีพี', 'กระบี่', 'เกาะที่มีธรรมชาติสวยงาม น้ำทะเลใส และดำน้ำชมปะการัง', 5, 'ตลอดวัน', 'ฟรี (ค่าทัวร์หรือเรือ)', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(12, 'อุทยานประวัติศาสตร์สุโขทัย', 'สุโขทัย', 'แหล่งโบราณคดีที่เคยเป็นเมืองหลวงของสุโขทัย', 2, '06:00 - 18:00', '20 บาท / 100 บาท', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(13, 'อุทยานประวัติศาสตร์อยุธยา', 'พระนครศรีอยุธยา', 'แหล่งมรดกโลกและเมืองเก่าแก่ของไทย', 2, '08:00 - 17:00', '20 บาท / 50 บาท', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(14, 'เขาสามมุก', 'ชลบุรี', 'จุดชมวิวทะเล และสถานที่ศักดิ์สิทธิ์', 4, 'ตลอดวัน', 'ฟรี', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(15, 'ปาย', 'แม่ฮ่องสอน', 'เมืองท่องเที่ยวยอดนิยมของนักเดินทาง มีธรรมชาติและวัฒนธรรมผสมผสาน', 6, 'ตลอดวัน', 'ฟรี', 'https://dummyjson.com/image/250', '2025-06-29 21:23:35', '2026-06-27 14:46:15', '1'),
+(16, 'อุนทยานผาแดงหลวง', 'ลำพูน', 'test test test', 2, '08:30-16:30', '500 บาท', NULL, '2025-08-22 22:55:43', '2025-08-22 22:55:43', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `is_editor` tinyint(1) NOT NULL DEFAULT 0,
+  `active` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `name`, `is_editor`, `active`) VALUES
+(1, 'admin@mail.com', 'YWRtaW4=', 'admin', 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `places`
+--
+ALTER TABLE `places`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `places`
+--
+ALTER TABLE `places`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
